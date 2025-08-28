170ae7d0ac4a6b5a4b3ba8dcac59e9b93ce02ff1
josefpguimaraes730 <apxflavinho@hotmail.com>
2025-08-28 12:20:26 -0300

    rename main class

---
+++ src/main/java/com/github/josefpguimaraes730/springclass/ProdutosAPI.java
@@ -0,0 +1,13 @@
+package com.github.josefpguimaraes730.springclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosAPI.class, args);
+	}
+
+}
